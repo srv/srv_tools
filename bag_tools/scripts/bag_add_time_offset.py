951ee1e1aa2e115cdd97c4af0b720fac2099d5ee
Stephan Wirth <stephan.wirth@uib.es>
2012-05-08 11:27:49 +0200

    added missing packages in manifest

--- bag_tools/scripts/bag_add_time_offset.py
+++ bag_tools/scripts/bag_add_time_offset.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python
 
-PKG = 'bagfile_utils' # this package name
+PKG = 'bag_tools' # this package name
 
 import roslib; roslib.load_manifest(PKG)
 import rospy
 import rosbag
-import sensor_msgs.msg
 import os
 import sys
+from optparse import OptionParser
 
-def fix_bagfile(inbag, outbag, left_info_url, right_info_url):
-    print 'Processing input bagfile : ', inbag
-    print 'Writing to output bagfile : ', outbag
+def fix_bagfile(inbag, outbag, topics, offset):
+    print '   Processing input bagfile: ', inbag
+    print '  Writing to output bagfile: ', outbag
+    print '            Changing topics: ', topics
+    print 'Changing publishing time by: ', offset
 
     outbag = rosbag.Bag(outbag, 'w')
 
-    time_offset = rospy.Duration(1e-6)
+    time_offset = rospy.Duration(offset)
+
+    count = {}
+    for topic in topics:
+      count[topic] = 0
 
     for topic, msg, t in rosbag.Bag(inbag).read_messages():
-        if topic == "MY_SUPER_TOPIC_WITH_WRONG_TIMESTAMPED_MESSAGES":
+        if topic in topics:
             outbag.write(topic, msg, t + time_offset)
+            count[topic] = count[topic] + 1
         else:
             outbag.write(topic, msg, t)
-    print 'Closing output bagfile and exit...'
+    print 'Closing output bagfile.'
     outbag.close()
+    print 'Changed the following:'
+    for k, v in count.iteritems():
+      print k, ': ', v, ' messages.'
 
 if __name__ == "__main__":
-    parser = OptionParser(usage="%prog INBAG OUTBAG",
-                          description='Create a new bagfile from an existing one shifting time by hardcoded offset.')
+    parser = OptionParser(usage="%prog INBAG OUTBAG OFFSET TOPICS",
+                          description='Shift the publishing time of given topics in input bagfile.')
     (options, args) = parser.parse_args()
-    if len(args) != 2:
+    if len(args) < 4:
         parser.error('Wrong number of arguments')
     inbag = args[0]
     outbag = args[1]
+    offset = args[2]
+    topics = []
+    for i in len(args) - 3:
+      topics.append(args[3+i])
     try:
-        fix_bagfile(inbag, outbag)
+        fix_bagfile(inbag, outbag, topics, offset)
     except Exception, e:
         import traceback
         traceback.print_exc()
