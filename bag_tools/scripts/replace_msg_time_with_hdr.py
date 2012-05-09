b2faba90dec305e44283b1b3e9e2ecb87f57b420
Miquel Massot <miquel.massot@uib.cat>
2012-05-09 14:36:01 +0200

    bugfixes

--- bag_tools/scripts/replace_msg_time_with_hdr.py
+++ bag_tools/scripts/replace_msg_time_with_hdr.py
@@ -5,30 +5,34 @@ PKG = 'bag_tools' # this package name
 import roslib; roslib.load_manifest(PKG)
 import rospy
 import rosbag
-import sensor_msgs.msg
 import os
 import sys
+from optparse import OptionParser
 
-def rpl_msg_time_with_hdr(inbag,outbag,in_topic):
-  for topic, msg, t in rosbag.Bag(inbag).read_messages():
+def rpl_msg_time_with_hdr(inbag,outbag):
+  print 'Processing input bagfile : ', inbag
+  print 'Writing to output bagfile : ', outbag
+  outbag = rosbag.Bag(outbag,'w')
+  for topic, msg, t in rosbag.Bag(inbag,'r').read_messages():
     # This also replaces tf timestamps under the assumption 
     # that all transforms in the message share the same timestamp
-    if topic == in_topic and msg.transforms:
+    if topic == "/tf" and msg.transforms:
       outbag.write(topic, msg, msg.transforms[0].header.stamp)
     else:
       outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+  print 'Closing output bagfile and exit...'
+  outbag.close();
 
 if __name__ == "__main__":
-  parser = OptionParser(usage="%prog INBAG OUTBAG TOPIC",
+  parser = OptionParser(usage="%prog INBAG OUTBAG",
                        description='Create a new bagfile from an existing one replacing the message time for the header time.')
   (options, args) = parser.parse_args()
-  if len(args) != 3:
+  if len(args) != 2:
     parser.error('Wrong number of arguments')
-    inbag = args[0]
-    outbag = args[1]
-    topic = args[2]
-    try:
-      rpl_msg_time_with_hdr(inbag, outbag,topic)
-    except Exception, e:
-      import traceback
-      traceback.print_exc()
+  inbag = args[0]
+  outbag = args[1]
+  try:
+    rpl_msg_time_with_hdr(inbag, outbag)
+  except Exception, e:
+    import traceback
+    traceback.print_exc()
