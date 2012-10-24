ac8ae40fb911fbc11ed0b02aea672fecf166b776
Stephan Wirth <stephan.wirth@uib.es>
2012-10-24 12:44:33 +0200

    added params for stdout and stderr files

--- launch_tools/src/executer.py
+++ launch_tools/src/executer.py
@@ -13,11 +13,14 @@ class Executer():
     """
     Simple class for executing a command in a subprocess.
     """
-    def __init__(self, command):
+    def __init__(self, command, stdout, stderr):
         self.command = shlex.split(command)
+        self.stdout = stdout
+        self.stderr = stderr
     def execute(self):
         rospy.loginfo("Launching process: \"%s\"...", " ".join(self.command))
-        self.process = subprocess.Popen(self.command)
+        rospy.loginfo("stdout/stderr: %s/%s", str(self.stdout), str(self.stderr))
+        self.process = subprocess.Popen(self.command, stdout=self.stdout, stderr=self.stderr)
         rospy.loginfo("Process launched.")
     def interrupt(self):
         if self.process.poll() is None: # check if process is still running
@@ -37,7 +40,11 @@ class Executer():
 if __name__ == "__main__":
     rospy.init_node('executer', anonymous=True)
     command = rospy.get_param("~command")
-    executer = Executer(command)
+    stdout_file = rospy.get_param("~stdout_file", "")
+    stderr_file = rospy.get_param("~stderr_file", "")
+    stdout = None if len(stdout_file) == 0 else open(stdout_file, 'w')
+    stderr = None if len(stderr_file) == 0 else open(stderr_file, 'w')
+    executer = Executer(command, stdout, stderr)
     executer.execute()
     rospy.on_shutdown(executer.interrupt)
     while executer.process.poll() is None and not rospy.is_shutdown():
