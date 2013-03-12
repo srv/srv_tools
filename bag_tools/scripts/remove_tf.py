16ba9c5091faf5b3efc1b19e1377c9b5f5dabb15
Stephan Wirth <swirth@willowgarage.com>
2013-03-12 10:02:40 -0700

    remove_tf now removes child tfs as well

--- bag_tools/scripts/remove_tf.py
+++ bag_tools/scripts/remove_tf.py
@@ -49,7 +49,7 @@ def remove_tf(inbag,outbag,frame_ids):
       if topic == "/tf":
           new_transforms = []
           for transform in msg.transforms:
-              if transform.header.frame_id not in frame_ids:
+              if transform.header.frame_id not in frame_ids and transform.child_frame_id not in frame_ids:
                   new_transforms.append(transform)
           msg.transforms = new_transforms
       outbag.write(topic, msg, t)
@@ -59,9 +59,9 @@ def remove_tf(inbag,outbag,frame_ids):
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser(
-      description='removes all transforms from the /tf topic that contain on of the given frame_ids in the header.')
-  parser.add_argument('-o', metavar='OUTPUT_BAGFILE', required=True, help='output bagfile')
+      description='removes all transforms from the /tf topic that contain one of the given frame_ids in the header as parent or child.')
   parser.add_argument('-i', metavar='INPUT_BAGFILE', required=True, help='input bagfile')
+  parser.add_argument('-o', metavar='OUTPUT_BAGFILE', required=True, help='output bagfile')
   parser.add_argument('-f', metavar='FRAME_ID', required=True, help='frame_id(s) of the transforms to remove from the /tf topic', nargs='+')
   args = parser.parse_args()
 
